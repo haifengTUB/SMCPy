added a norm_log_weight attribute to particles to facilitate normalizing constant calcs; also converted attributes to properties and fixed tests accodringly

smcpy/smc/smc_sampler.py
@@ -137,8 +137,8 @@ class SMCSampler(Properties):
         self.autosaver = autosave_file
         self.restart_time_step = restart_time_step
 
-        start_time_step = 1
         if self.restart_time_step == 1:
+            start_time_step = 1
             initializer = ParticleInitializer(self._mcmc, self.temp_schedule,
                                               self._comm)
             initializer.set_proposal_distribution(proposal_center,
@@ -232,7 +232,7 @@ class SMCSampler(Properties):
         if self._rank == 0:
             step = SMCStep()
             step.set_particles(np.concatenate(particles))
-            step.normalize_step_log_weights()
+            step.normalize_log_weights()
         else:
             step = None
         return step

tests/test_smc_step.py
@@ -67,19 +67,14 @@ def test_get_covariance(linear_step):
     arr_alm_eq(linear_step.get_covariance(), exp_cov)
 
 
-def test_normalize_step_log_weights(mixed_weight_step):
-    mixed_weight_step.normalize_step_log_weights()
-    for index, p in enumerate(mixed_weight_step.particles):
-        p.log_weight = np.exp(p.log_weight)
-    assert sum(mixed_weight_step.get_log_weights()) == 1
-
-
-def test_normalize_step_weights(mixed_weight_step):
-    normalized_weights = mixed_weight_step.normalize_step_weights()
+def test_get_normalized_step_weights(mixed_weight_step):
+    mixed_weight_step.normalize_log_weights()
+    normalized_weights = mixed_weight_step.get_norm_weights()
     assert sum(normalized_weights) == 1
 
 
 def test_compute_ess(filled_step):
+    filled_step.normalize_log_weights()
     assert filled_step.compute_ess() == pytest.approx(5.0)
 
 